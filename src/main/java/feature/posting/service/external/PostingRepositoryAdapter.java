319b7c23fd8945a3be1e703b3ee857003659d6d3
cpdmpara <leebadagyumin0129@gmail.com>
2026-02-14 23:23:19 +0900

    fix: PostingRepositoryAdapter에 getById, getList, update, deleteById 추가

--- src/main/java/feature/posting/service/external/PostingRepositoryAdapter.java
+++ src/main/java/feature/posting/service/external/PostingRepositoryAdapter.java
@@ -4,4 +4,8 @@ import feature.posting.domain.Posting;
 
 public interface PostingRepositoryAdapter {
     Posting save(Posting posting);
+    Posting getById(Long id);
+    Posting getList(Long page, Long pageSize);
+    Posting update(Posting changedPosting);
+    void deleteById(Long id);
 }
